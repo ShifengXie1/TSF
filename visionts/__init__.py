c503abdf8ad8b8e7b7ea4e8f7e0f222f72384cda
xsf <12025215132@stu.ynu.edu.cn>
2026-03-29 22:11:48 +0800

    修复VisionTSpp注释bug

--- visionts/__init__.py
+++ visionts/__init__.py
@@ -1,7 +1,14 @@
-from .model import VisionTS, VisionTSpp
+from .model import VisionTS
 from .util import freq_to_seasonality_list
+
+try:
+    from .model import VisionTSpp
+except ImportError:
+    VisionTSpp = None
 
 __version__ = "1.0.0"
 __author__ = "Mouxiang Chen, Lefei Shen"
 
-__all__ = ["VisionTS", "VisionTSpp", "freq_to_seasonality_list"]
+__all__ = ["VisionTS", "freq_to_seasonality_list"]
+if VisionTSpp is not None:
+    __all__.append("VisionTSpp")
