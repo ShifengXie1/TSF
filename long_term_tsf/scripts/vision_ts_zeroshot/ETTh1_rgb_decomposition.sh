f11968e0b85ed2a18ab52911b7f021a13efbee71
xsf <12025215132@stu.ynu.edu.cn>
2026-03-30 19:11:26 +0800

    增加zeroshot脚本rgb_decomposition

---
+++ long_term_tsf/scripts/vision_ts_zeroshot/ETTh1_rgb_decomposition.sh
@@ -0,0 +1,36 @@
+export CUDA_VISIBLE_DEVICES=3
+export CKPT_DIR="../ckpt/"
+export VM_ARCH="mae_base"
+export CONTEXT_LEN=2880
+export PERIODICITY=24
+export ALIGN_CONST=0.4
+export NORM_CONST=0.4
+export RGB_MA_KERNEL=25
+export RGB_DYNAMIC_SCALE_MODE="sample"
+export RGB_SCALE_EPS=1e-5
+
+for PRED_LEN in 96 192 336 720; do
+    python -u run.py \
+    --task_name long_term_forecast \
+    --is_training 1 \
+    --model VisionTS \
+    --root_path ./dataset/ETT-small/ \
+    --data_path ETTh1.csv \
+    --save_dir save/ETTh1_rgb_decomposition_$PRED_LEN \
+    --model_id VisionTSRGB_ETTh1_$PRED_LEN \
+    --data ETTh1 \
+    --features M \
+    --train_epochs 0 \
+    --vm_arch $VM_ARCH \
+    --vm_ckpt $CKPT_DIR \
+    --seq_len $CONTEXT_LEN \
+    --periodicity $PERIODICITY \
+    --pred_len $PRED_LEN \
+    --norm_const $NORM_CONST \
+    --align_const $ALIGN_CONST \
+    --rgb_mode decomposition \
+    --rgb_ma_kernel $RGB_MA_KERNEL \
+    --rgb_channel_scales 1.0 1.0 1.0 \
+    --rgb_dynamic_scale_mode $RGB_DYNAMIC_SCALE_MODE \
+    --rgb_scale_eps $RGB_SCALE_EPS
+done;
